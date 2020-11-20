376ed0148c922bf1cacfdf5be0f5d4f7f1b7e08d
emtalen <59577366+emtalen@users.noreply.github.com>
2020-11-20 13:00:54 +0100

    trying to figure out the 401 error

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -2,9 +2,11 @@ class Api::V1::PerformanceDataController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    binding.pry
     data = PerformanceData.new(performance_data_params.merge!(user: current_user))
 
     if data.save
+      binding.pry
       head :ok
     else
       render json: { error: data.errors.full_messages }
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  def authenticate_user!
+    binding.pry
+    super
+  end
 end
